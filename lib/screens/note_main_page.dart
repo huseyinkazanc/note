2f82042a8b303de88af09bf5f2e69975f7898e7e
Hüseyin Kazanç <huseyinkazanc2@gmail.com>
2024-04-08 21:01:01 +0300

    first commit

---
+++ lib/screens/note_main_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NoteMainPage extends StatefulWidget {
+  const NoteMainPage({super.key});
+
+  @override
+  State<NoteMainPage> createState() => _NoteMainPageState();
+}
+
+class _NoteMainPageState extends State<NoteMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Column(
+        children: [],
+      ),
+    );
+  }
+}
