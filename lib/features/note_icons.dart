010e9ec7247fab35bebd7888d4544bb47f12df1b
Hüseyin Kazanç <huseyinkazanc2@gmail.com>
2024-04-12 02:55:50 +0300

    Added NoteIcons class with static IconData constants for search, add, list grid page, and main show dialog icons

---
+++ lib/features/note_icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NoteIcons {
+  static const IconData icSearch = Icons.search;
+  static const IconData icAdd = Icons.add;
+  static const IconData iCLstGrdPage = Icons.check_box;
+  static const IconData icMainShwDlg = Icons.delete;
+}
