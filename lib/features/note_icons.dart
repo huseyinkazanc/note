d39c20e972798767c0e024f831abb3db665c82ea
Hüseyin Kazanç <huseyinkazanc2@gmail.com>
2024-04-13 16:42:31 +0300

    Add new icon for list grid page trailing in NoteIcons class

--- lib/features/note_icons.dart
+++ lib/features/note_icons.dart
@@ -4,5 +4,6 @@ class NoteIcons {
   static const IconData icSearch = Icons.search;
   static const IconData icAdd = Icons.add;
   static const IconData iCLstGrdPage = Icons.check_box;
+  static const IconData icLstGrdPage_trailing = Icons.edit_document;
   static const IconData icMainShwDlg = Icons.delete;
 }
