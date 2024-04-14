8b30cce9b87d52319063f0528000e15b764c5658
Hüseyin Kazanç <huseyinkazanc2@gmail.com>
2024-04-14 23:35:03 +0300

    Refactor NoteMainPage to use NoteGeneralContent class for storing messages and separate lists for messagesTitle and messagesExplain

--- lib/widgets/note_widget_gridView.dart
+++ lib/widgets/note_widget_gridView.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:note/features/note_colors.dart';
-import 'package:note/features/note_font.dart';
 import 'package:note/features/note_icons.dart';
-import 'package:note/features/note_strings.dart';
+import 'package:note/notecontent/note_general_content.dart';
 
 class NoteWidgetGridView extends StatefulWidget {
-  const NoteWidgetGridView(
-      {super.key,
-      required this.messagesTitle,
-      required this.messageColors,
-      this.onLongPress,
-      this.onTap,
-      required this.messagesExplain});
+  const NoteWidgetGridView({
+    super.key,
+    required this.messages,
+    required this.messageColors,
+    this.onLongPress,
+    this.onTap,
+  });
 
-  final List<String> messagesTitle;
-  final List<String> messagesExplain;
+  final List<NoteGeneralContent> messages;
   final Map<String, Color> messageColors;
-  final Function(String)? onLongPress;
-  final Function(String)? onTap;
+  final Function(NoteGeneralContent)? onLongPress;
+  final Function(NoteGeneralContent)? onTap;
 
   @override
   State<NoteWidgetGridView> createState() => _NoteWidgetGridViewState();
@@ -25,90 +22,11 @@ class NoteWidgetGridView extends StatefulWidget {
 
 class _NoteWidgetGridViewState extends State<NoteWidgetGridView> {
   final Map<String, bool> _isCheckedMap = {};
-  final Map<String, Color> messageColors = {};
 
+  // Below method is to get the text color based on the background color
   Color getTextColor(Color backgroundColor) {
     final luminance = backgroundColor.computeLuminance();
     return luminance > 0.5 ? Colors.black : Colors.white;
-  }
-
-  /* @override
-  void initState() {
-    super.initState();
-    for (final message in widget.messages) {
-      _isCheckedMap[message] = false;
-    }
-  }*/
-
-  void _AlertDialogEdit(String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          backgroundColor: NoteColors.darkBgColor,
-          title: const Text('Edit Message'),
-          content: SizedBox(
-            height: 250,
-            child: Column(
-              children: [
-                TextFormField(
-                  initialValue: message,
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                        color: NoteColors.whiteColor,
-                      ),
-                  decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                      color: NoteColors.whiteColor,
-                    )),
-                    hintText: NoteStrings.appAlrtDlgTitlHnt,
-                    hintStyle: Theme.of(context).textTheme.titleLarge?.copyWith(
-                          color: NoteColors.whiteColor,
-                        ),
-                  ),
-                  onChanged: (value) {
-                    // Handle the changes
-                  },
-                ),
-                TextFormField(
-                  initialValue: message,
-                  maxLines: 7,
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                        color: NoteColors.whiteColor,
-                        fontSize: NoteFont.fontSizeEighteen,
-                      ),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: NoteStrings.appAlrtDlgSbjHnt,
-                    hintStyle: Theme.of(context).textTheme.titleLarge?.copyWith(
-                          color: NoteColors.whiteColor,
-                          fontSize: NoteFont.fontSizeEighteen,
-                        ),
-                  ),
-                  onChanged: (value) {
-                    // Handle the changes
-                  },
-                ),
-              ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        );
-      },
-    );
   }
 
   @override
@@ -120,17 +38,18 @@ class _NoteWidgetGridViewState extends State<NoteWidgetGridView> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ...widget.messagesTitle.map((messageTitle) {
-                // Get the color for the current message from the messageColors map
-                final color = widget.messageColors[messageTitle];
-                final textColor = getTextColor(color!);
+              // Below is the header of the grid view
+              ...widget.messages.map((noteContent) {
+                // Below is to get the color of the message based on the title
+                final color = widget.messageColors[noteContent.messageTitle]!;
+                // Below is to get the text color based on the background color
+                final textColor = getTextColor(color);
                 return GestureDetector(
+                  // Below onLongPress is to get the long press event
                   onLongPress: () {
-                    setState(() {
-                      if (widget.onLongPress != null) {
-                        widget.onLongPress!(messageTitle);
-                      }
-                    });
+                    if (widget.onLongPress != null) {
+                      widget.onLongPress!(noteContent);
+                    }
                   },
                   child: Card(
                     shape: RoundedRectangleBorder(
@@ -138,31 +57,33 @@ class _NoteWidgetGridViewState extends State<NoteWidgetGridView> {
                       borderRadius: BorderRadius.circular(40.0),
                     ),
                     child: ListTile(
-                      tileColor: color, // Use the color obtained from the messageColors map
+                      tileColor: color,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(40.0),
                       ),
                       title: Text(
-                        messageTitle,
+                        noteContent.messageTitle,
                         style: TextStyle(color: textColor),
                       ),
-                      subtitle: Text(messageTitle),
+                      subtitle: Text(noteContent.messageContent),
                       leading: Checkbox(
                         activeColor: color,
                         side: BorderSide(
                           color: textColor,
                           width: 2.0,
                         ),
-                        value: _isCheckedMap[messageTitle] ?? false,
+                        value: _isCheckedMap[noteContent.messageTitle] ?? false,
                         onChanged: (newValue) {
                           setState(() {
-                            _isCheckedMap[messageTitle] = newValue ?? false;
+                            _isCheckedMap[noteContent.messageTitle] = newValue ?? false;
                           });
                         },
                       ),
                       trailing: GestureDetector(
                         onTap: () {
-                          _AlertDialogEdit(messageTitle);
+                          if (widget.onTap != null) {
+                            widget.onTap!(noteContent);
+                          }
                         },
                         child: Icon(
                           NoteIcons.icLstGrdPage_trailing,
