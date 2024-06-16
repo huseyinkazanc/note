4e8ca5cb272919416b6684f34817423de4c2a120
Hüseyin Kazanç <huseyinkazanc2@gmail.com>
2024-06-16 22:32:32 +0300

    feat: Add noteColor field to NoteGeneralContent class

--- lib/notecontent/note_general_content.dart
+++ lib/notecontent/note_general_content.dart
@@ -1,11 +1,15 @@
+import 'dart:ui';
+
 class NoteGeneralContent {
   final int id;
   String messageTitle;
   String messageContent;
+  Color? noteColor; // Eklenen alan
 
   NoteGeneralContent({
     required this.id,
     required this.messageTitle,
     required this.messageContent,
+    this.noteColor, // Renk alanı opsionel olarak tanımlandı
   });
 }
