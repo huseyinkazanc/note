be47fa14e4f44e61041187d5cecc4b53fbe5146d
Hüseyin Kazanç <huseyinkazanc2@gmail.com>
2024-06-18 16:21:14 +0300

    Refactor variable and function names for consistency in note_widget_list.dart and note_widget_list_edit.dart, update maxLines for TextFormField in NoteWidgetExplainMessage, and add Login Page strings

--- lib/features/note_strings.dart
+++ lib/features/note_strings.dart
@@ -8,4 +8,21 @@ class NoteStrings {
   static const String appCreateAlrTxt = 'Whick one do you want to create?';
   static const String appCreateAlrQuckTxt = 'Quick Note';
   static const String appCreateAlrDtlTxt = 'Detail Note';
+  // Login Page
+  static const String appLginTxt = 'Log in to continue.';
+  static const String appLginSgnTxt = 'Sign Up to continue.';
+  static const String appLginHntMail = 'Enter your email...';
+  static const String appLginHntPsw = 'Enter your password...';
+  static const String appLginUsrNmHnt = 'Username';
+  static const String appLginUsrEml = 'Email';
+  static const String appLginUsrPsw = 'Password';
+  static const String appLginUsrCnfrPsw = 'Confirm Password';
+  static const String appLginRstPsw = 'Reset Password';
+  static const String appLginRmbr = 'Remember me';
+  static const String appLginBtnTxt = 'LOG IN';
+  static const String appLginOr = 'Or';
+  static const String appLginFrgtPsw = 'Forgot Password?';
+  static const String appLginWthGog = 'Continue with Google';
+  static const String appLginDntAcnt = 'Don’t have an account?';
+  static const String appLginSgnUp = 'Sign Up';
 }
